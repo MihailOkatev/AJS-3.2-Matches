4694f4cb84abd38d5363b0df5370bf994134764d
Astario <mokatev@yandex.ru>
2020-05-18 13:45:16 +0300

    similar health test modifed

--- src/js/func.js
+++ src/js/func.js
@@ -1,5 +1,22 @@
 export default function sortByHealth(sqaude) {
   const copyOfSquade = sqaude.slice();
-  copyOfSquade.sort((a, b) => (a.health < b.health ? 1 : -1));
+  copyOfSquade.sort((a, b) => {
+    if (a.health < b.health) {
+      return 1;
+    }
+    if (a.health > b.health) {
+      return -1;
+    }
+    if (a.health === b.health) {
+      let aName = a.name;
+      let bName = b.name;
+      if (aName > bName) {
+        return 1;
+      }
+      if (aName < bName) {
+        return -1;
+      }
+    }
+  });
   return copyOfSquade;
 }
